97f64d8c8cb9379675de9fc0e8a643f1c39cb085
joaocferreira <f16123@gmail.com>
2016-04-10 10:35:08 +0200

    create generator

---
+++ README.md
@@ -0,0 +1,9 @@
+# base_generator
+
+---
+
+### Steps
+
+1. Clone
+2. npm install
+3. gulp dev
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream');
+
+gulp.task('sass', function() {
+    return gulp.src('./app/styles/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./app/styles'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+})
+
+gulp.task('cleanCSS', function() {
+    return gulp.src('./app/styles/main.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/styles'))
+})
+
+gulp.task('server--dev', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
+})
+
+gulp.task('server--dist', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    })
+})
+
+gulp.task('browserify', function() {
+    return browserify('./app/scripts/main.js')
+        .bundle()
+        .on('error', function(e) {
+            gutil.log(e);
+        })
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./app/scripts'))
+})
+
+gulp.task('copy', function() {
+    return gulp.src('./app/*.html')
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('uglify', function() {
+    return gulp.src('./app/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+});
+
+gulp.task('dev', ['server--dev', 'sass'], function() {
+    gulp.watch('./app/styles/*.scss', ['sass']);
+    gulp.watch('./app/*.html', browserSync.reload);
+    gulp.watch('./app/scripts/*js', browserSync.reload);
+})
+
+gulp.task('build', ['sass', 'browserify', 'cleanCSS', 'copy', 'uglify', 'server--dist']);
